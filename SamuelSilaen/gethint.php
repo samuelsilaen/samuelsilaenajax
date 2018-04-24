136e3289a8ff818331dadf78784123515acfde49
Samuel Silaen <38452954+samuelsilaen@users.noreply.github.com>
2018-04-24 15:37:32 +0700

    justlikett

---
+++ SamuelSilaen/gethint.php
@@ -0,0 +1,30 @@
+<?php
+
+$a[] = "Murad";
+$a[] = "Algi Tamvan";
+$a[] = "Valhein";
+$a[] = "Zanis";
+$a[] = "LuBu";
+$a[] = "Max";
+
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+if ($q !== "") {
+	$q = strtolower($q);
+	$len=strlen($q);
+	foreach($a as $name) {
+		if (stristr($q, substr($name, 0, $len))){
+			if ($hint === ""){
+				$hint = $name;
+			} else {
+				$hint .= ", $name";
+			
+			}
+		}
+	}
+}
+
+echo $hint === "" ? "no suggestion" : $hint;
+?>
